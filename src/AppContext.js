dde848fe782fa7746b47cef3d07502d7561a41ef
Pranav Negandhi <pranav@vertika.tv>
2023-06-19 20:10:01 +0530

    Replaced the implementation that uses logic gates and other binary constructs with a straightforward decimal-to-binary conversion algorithm.

---
+++ src/AppContext.js
@@ -0,0 +1,130 @@
+const AppContext = function () {
+    const buttonClearAll = document.querySelector('#buttonClearAll');
+    buttonClearAll.addEventListener('click', () => clear());
+
+    const buttonDigitOne = document.querySelector('#buttonDigitOne');
+    buttonDigitOne.addEventListener('click', () => appendDigit('1'));
+
+    const buttonDigitZero = document.querySelector('#buttonDigitZero');
+    buttonDigitZero.addEventListener('click', () => appendDigit('0'));
+
+    const buttonOperatorAdd = document.querySelector('#buttonOperatorAdd');
+    buttonOperatorAdd.addEventListener('click', () => appendOperator('+'));
+
+    const buttonEquals = document.querySelector('#buttonOperatorEquals');
+    buttonEquals.addEventListener('click', () => compute());
+
+    const digitDisplay = document.querySelector('#digitDisplay');
+
+    let _infix = [];
+
+    let _display;
+
+    let _operation = null;
+
+    let _operand = null;
+
+    const clear = function () {
+        _infix.splice(0, _infix.length);
+        setDisplay('0');
+        _operation = null;
+        _operand = null;
+    };
+
+    const getDisplay = () => _display;
+
+    const setDisplay = (value) => {
+        _display = value.toString();
+        digitDisplay.value = _display;
+    };
+
+    const appendDigit = function (value) {
+        if (null != _operation) {
+            setDisplay('0');
+
+            /// Save the operator in the infix expression
+            _infix.push(_operation);
+            _operation = null;
+        }
+
+        /// Three-step process to display the number without leading zeros
+        /// 1. Append the digit to the end of the display
+        /// 2. Evaluate it as an int
+        /// 3. Convert the result to a string and display it on screen
+        var temp = getDisplay() + value;
+        var result = parseInt(temp);
+        /// Display the binary number
+        setDisplay(result);
+
+        /// Convert the binary number to decimal
+        result = parseInt(temp, 2);
+        /// Create an operand token and store in a field
+        /// to be added later to the end of the infix list
+        _operand = result;
+    }
+
+    const appendOperator = function (operator) {
+        /// Check input values before allowing the operation
+        if (null != _operation || null == _operand) {
+            return;
+        }
+
+        if ('+' == operator) {
+            _operation = '+';
+
+            _infix.push(_operand);
+            _operand = null;
+        } else if ('-' == operator) {
+            _operation = '-';
+
+            _infix.push(_operand);
+            _operand = null;
+        }
+    };
+
+    const compute = function () {
+        if (null != _operation || null == _operand) {
+            return;
+        }
+
+        _infix.push(_operand);
+        _operand = null;
+
+        var postfix = [];
+        var stack = [];
+
+        for (var i = 0; i < _infix.length; i++) {
+            var token = _infix[i];
+
+            if (token !== '+' && token !== '-') {
+                postfix.push(token);
+            } else if (token === '+' || token === '-') {
+                while (stack.length > 0) {
+                    var d = stack.pop();
+                    postfix.push(d);
+                }
+
+                stack.push(token);
+            }
+        }
+
+        while (stack.push > 0) {
+            var d = stack.Pop();
+            postfix.Add(d);
+        }
+
+        clear();
+
+        console.log(postfix);
+
+        // var result = _model.Evaluate(postfix);
+        // Display = Convert.ToString(result, 2);
+    }
+
+    setDisplay('0');
+
+    return {
+        getDisplay: getDisplay,
+        setDisplay: setDisplay
+    }
+};
